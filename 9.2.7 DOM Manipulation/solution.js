b11e310142d779928cc1edfbce2ad7ce11676210
Topgyal Gurung <topgyaltsering3@gmail.com>
2024-08-10 23:48:07 -0400

    added eventlistener for dom ex

--- 9.2.7 DOM Manipulation/solution.js
+++ 9.2.7 DOM Manipulation/solution.js
@@ -1,41 +1,55 @@
+// wait for DOM to load then load script tag to execute JS
+document.addEventListener("DOMContentLoaded", function () {
+  solveTask1();
+  solveTask2();
+  solveTask3();
+  solveTask4();
+  solveTask5();
+  solveTask6();
+  solveTask7();
+  solveTask8();
+  solveTask9();
+});
+
+/*
+// to have it fully loaded
+window.addEventListener(‘load’, function(){
+		console.log(‘FULLY LOADED’);
+	});
+
+*/
+
 function solveTask1() {
   let task1 = document.getElementById("task1");
   task1.innerText = "Changed using 'innerText'.";
 }
-solveTask1();
 
 function solveTask2() {
   let task2 = document.getElementById("task2");
   task2.innerHTML = "<button> Submit </button>";
 }
-solveTask2();
 
 function solveTask3() {
   document.body.style.backgroundColor = "#232323";
 }
-solveTask3();
 
 function solveTask4() {
   document.querySelectorAll(".item").forEach((item) => {
     item.style.border = "5px solid orange";
   });
 }
-solveTask4();
 
 function solveTask5() {
   document.getElementById("task5").href = "https://www.springboard.com/";
 }
-solveTask5();
 
 function solveTask6() {
   document.getElementById("task6").value = "DOM Master";
 }
-solveTask6();
 
 function solveTask7() {
   document.getElementById("task7").classList.add("new-class");
 }
-solveTask7();
 
 function solveTask8() {
   let newBtn = document.createElement("button");
@@ -43,10 +57,8 @@ function solveTask8() {
   document.getElementById("task8").appendChild(newBtn);
   //   document.getElementById("task8").innerHTML = "<button> hi </button>";
 }
-solveTask8();
 
 function solveTask9() {
   let element = document.getElementById("task9");
   element.parentNode.removeChild(element);
 }
-solveTask9();
